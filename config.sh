22bc5cd973cb56dd7ec3d2b5da8d6f79805da961
yangxins <yangmame@icloud.com>
2017-04-24 15:39:07 +0800

    first

--- config.sh
+++ config.sh
@@ -5,7 +5,7 @@ hwclock --systohc --utc
 echo zh_CN.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
 echo LANG=zh_CN.UTF-8 > /etc/locale.conf
-read -p "input your hostname: " HOSTNAME
+read -p "input your hostname:  " HOSTNAME
 echo $HOSTNAME  > /etc/hostname
 echo change your root passwd
 passwd
@@ -15,19 +15,19 @@ then pacman -S grub efibootmger -y
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 else fdisk -l
-read -p "input the disk you want to install the grub" GRUB
+read -p "input the disk you want to install the grub  " GRUB
 grub-install --target=i386-pc $GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 ##安装显卡驱动
 VIDEO=4
 while (($VIDEO!=1&&$VIDEO!=2&&VIDEO!=3));do
-read -p "what is your video card ? (input the number: intel-1 nvidia-2 intel/nvidia-3 " VIDEO
+read -p "what is your video card ? (input the number: intel-1 nvidia-2 intel/nvidia-3  " VIDEO
 if (($VIDEO == 1))
 then pacman -S xf86-video-intel -y
 elif (($VIDEO == 2))
 then TMP=4
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
-read -p "version of nvidia-driver to install (input the number, 1--GeForce-8 and newer 2--GeForce-6/7 3--older" TMP
+read -p "version of nvidia-driver to install (input the number, 1--GeForce-8 and newer 2--GeForce-6/7 3--older  " TMP
 if (($TMP==1))
 then pacman -S nvidia -y
 elif (($TMP==2))
@@ -42,7 +42,7 @@ then pacman -S bumbelbee -y
 systemctl enable bumbelbeed
 TMP=4
 while (($TMP!=1&&$TMP!=2&&$TMP!=3));do
-read -p "version of nvidia-driver to install (input the number, 1--GeForce-8 and newer 2--GeForce-6/7 3--older" TMP
+read -p "version of nvidia-driver to install (input the number, 1--GeForce-8 and newer 2--GeForce-6/7 3--older   " TMP
 if (($TMP==1))
 then pacman -S nvidia -y
 elif (($TMP==2))
@@ -61,7 +61,7 @@ echo "[archlinuxcn]
 Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
 pacman -Syu&&pacman -S archlinuxcn-keyring&&pacman -S iw wpa_supplicant dialog networkmanager xorg-server xterm firefox yaourt
 systemctl enable NetworkManager
-read -p "do you have bluetooth ? (y or enter " TMP
+read -p "do you have bluetooth ? (y or enter  " TMP
 if (("$TMP"=="y"))
 then pacman -S bluez blueman&&systemctl enable bluetooth
 fi
@@ -69,7 +69,7 @@ fi
 echo "which desktop you want to install"
 DESKTOP=9
 while (($DESKTOP!=1&&$DESKTOP!=2&&$DESKTOP!=3&&$DESKTOP!=4&&$DESKTOP!=5&&$DESKTOP!=6&&$DESKTOP!=7&&$DESKTOP!=8));do
-read -p "gnome-1 kde-2 lxde-3 lxqt-4 mate-5 xfce-6 deepin-7 budgie-8" DESKTOP
+read -p "gnome-1 kde-2 lxde-3 lxqt-4 mate-5 xfce-6 deepin-7 budgie-8   " DESKTOP
 case $DESKTOP in
     1) pacman -S gnome -y&&systemctl enable gdm
     ;;
--- live.sh
+++ live.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 ##分区
-read -p "do you want to adjust the partition ? (input y to use fdisk or enter to continue: " TMP
+read -p "do you want to adjust the partition ? (input y to use fdisk or enter to continue:  " TMP
 if (("$TMP" == "y"))
 then fdisk -l
-read -p "which disk do you want to partition ? (/dev/sdX: " DISK
+read -p "which disk do you want to partition ? (/dev/sdX:  " DISK
 fdisk $DISK
 fi
 fdisk -l
-read -p "input the / mount point: " ROOT
-read -p "format it ? (y or enter " TMP
+read -p "input the / mount point:  " ROOT
+read -p "format it ? (y or enter  " TMP
 if (("$TMP"=="y"))
-then read -p "enter 1 to use ext4 defalut to use btrfs" TMP
+then read -p "enter 1 to use ext4 defalut to use btrfs  " TMP
 if (($TMP==1))
 then mkfs.ext4 $ROOT
 else mkfs.btrfs $ROOT
 fi
 mount $ROOT /mnt
 fi
-read -p "do you have the /boot mount point? (y or enter " BOOT
+read -p "do you have the /boot mount point? (y or enter  " BOOT
 if (("$BOOT" == "y"))
-then read -p "input the /boot mount point: " BOOT
-read -p "format it ? (y or enter " TMP
+then read -p "input the /boot mount point:  " BOOT
+read -p "format it ? (y or enter  " TMP
 if (("$TMP"=="y"))
 then mkfs.fat -F32 $ROOT
 fi
 mkdir /mnt/boot
 mount $BOOT /mnt/boot
 fi
-echo "do you have the swap partition ? (y or enter " SWAP
+echo "do you have the swap partition ? (y or enter  " SWAP
 if (("$SWAP" == "y"))
-then read -p "input the swap mount point: " SWAP
-read -P "format it ? (y or enter " TMP
+then read -p "input the swap mount point:  " SWAP
+read -P "format it ? (y or enter  " TMP
 if (("$TMP"=="y"))
 then mkswap $SWAP
 fi
