810d26c4e50b07b967d6d16592f958dbd36718f4
yangxins <yangmame@icloud.com>
2017-04-24 14:37:32 +0800

    first

--- live.sh
+++ live.sh
@@ -8,16 +8,32 @@ fdisk $DISK
 fi
 fdisk -l
 read -p "input the / mount point: " ROOT
+read -p "format it ? (y or enter " TMP
+if (("$TMP"=="y"))
+then read -p "enter 1 to use ext4 defalut to use btrfs" TMP
+if (($TMP==1))
+then mkfs.ext4 $ROOT
+else mkfs.btrfs $ROOT
+fi
 mount $ROOT /mnt
+fi
 read -p "do you have the /boot mount point? (y or enter " BOOT
 if (("$BOOT" == "y"))
 then read -p "input the /boot mount point: " BOOT
+read -p "format it ? (y or enter " TMP
+if (("$TMP"=="y"))
+then mkfs.fat -F32 $ROOT
+fi
 mkdir /mnt/boot
 mount $BOOT /mnt/boot
 fi
 echo "do you have the swap partition ? (y or enter " SWAP
 if (("$SWAP" == "y"))
 then read -p "input the swap mount point: " SWAP
+read -P "format it ? (y or enter " TMP
+if (("$TMP"=="y"))
+then mkswap $SWAP
+fi
 swapon $SWAP
 fi
 ##更改软件源
